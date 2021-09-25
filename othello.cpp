38b4061e66b7ae168a0d134915a68973def7237d
Aniket Gattani <aniketgattani@gmail.com>
2021-09-25 18:37:21 -0500

    Adding comments

--- othello.cpp
+++ othello.cpp
@@ -4,10 +4,10 @@
 #include <stdlib.h>
 #include <math.h>
 #include <iostream>
-#include <climits>
+#include <climits> // for UULONG_MAX
 #include <cilk/cilk_api.h>
-#include "timer.h"
-#include <utility>
+#include "timer.h" // to calculate time taken by program in UNIX
+#include <utility> // for using std::pair
 using namespace std;
 
 #define BIT 0x1
@@ -16,9 +16,9 @@ using namespace std;
 #define O_WHITE 1
 #define OTHERCOLOR(c) (1-(c))
 
-#define HUMAN 'h'
-#define COMPUTER 'c'
-#define CHUNK_SIZE 3
+#define HUMAN 'h' // to identify player as human
+#define COMPUTER 'c' // to identify player as computer 
+#define CHUNK_SIZE 3 // for truncating some parallel execution to increase chunk size
 
 #define BOARD_BIT_INDEX(row,col) ((8 - (row)) * 8 + (8 - (col)))
 #define BOARD_BIT(row,col) (0x1LL << BOARD_BIT_INDEX(row,col))
@@ -52,14 +52,31 @@ typedef struct { ull disks[2]; } Board;
 
 typedef struct { int row; int col; } Move;
 
+
+/*
+	Player class that recognizes each player in the Reversi game
+	- type - to identify if the player is computer or human
+	- color - to identify the color chosen by the player X or O
+	- move_possible - to identify if there is a further move is possible by the player
+	- depth - (only for computer player) The search depth to evaluate which maximizes its winning probability 
+*/
 typedef struct { char type; int color; bool move_possible; int depth;} Player;
+
+
+/*
+	Comparator for the reducer. The reducer used is reducer_max. 
+	- The reducer consists of a pair of (Move, diff)
+	- diff is the difference between the number of disks of player vs opponent
+	- Move is the move which leads to that diff
+	- We have to maximize the diff
+	- In case difference is the same, we choose the move which has highest row and col to enforce determinism
+*/
 
 class MoveComparison {
 public:
   bool operator()(const pair<Move, int>& p1, const pair<Move, int>& p2) const {
 	Move m1 = p1.first;
 	Move m2 = p2.first;
-	//cout<<m1.row<<' '<<m1.col<<' '<<p1.second<<' '<<m2.row<<' '<<m2.col<<' '<<p2.second<<"lahude"<<endl;
 	if(p1.second != p2.second) return p1.second < p2.second;
 	if(m1.row != m2.row) return m1.row < m2.row;
 	return (m1.col < m2.col);
@@ -81,7 +98,7 @@ Move offsets[] = {
 int noffsets = sizeof(offsets)/sizeof(Move);
 char diskcolor[] = { '.', 'X', 'O', 'I' };
 
-
+/* helper functions to print board configuration */
 void PrintDisk(int x_black, int o_white)
 {
 	printf(" %c", diskcolor[x_black + (o_white << 1)]);
@@ -126,6 +143,16 @@ void PlaceOrFlip(Move m, Board *b, int color)
 	try to flip disks along a direction specified by a move offset.
 	the return code is 0 if no flips were done.
 	the return value is 1 + the number of flips otherwise.
+	
+	Examples of a valid flip:
+		Let there be the following order of disks in the given offset direction (color = X):
+		. O O X
+		if we place X at the leftmost position:
+		X X X X
+	
+	Examples of invalid flip (color = X):
+		1. . O O O
+		2. . O . X
 */
 int TryFlips(Move m, Move offset, Board *b, int color, int verbose, int domove)
 {
@@ -138,15 +165,18 @@ int TryFlips(Move m, Move offset, Board *b, int color, int verbose, int domove)
 	Board boardAfterMove = *b;
 	while(!IS_MOVE_OFF_BOARD(next)){
 		ull nextbit = MOVE_TO_BOARD_BIT(next);
+		/* you should flip a disk only if it already has a disk of the opponent */
 		if (nextbit & boardAfterMove.disks[OTHERCOLOR(color)]) {
 			nflips++;
 			if (verbose) printf("flipping disk at %d,%d\n", next.row, next.col);
 			if (domove) PlaceOrFlip(next, &boardAfterMove, color);
 		} 
+		/* if you have finally encountered a disk of the same color then we should stop*/
 		else if (nextbit & boardAfterMove.disks[color]) {
 			*b = boardAfterMove;
 			return nflips;
 		}
+		/* all disks between a flip should be already occupied. Else there can't be any flip */
 		else return 0;
 		next.row += offset.row;
 		next.col += offset.col;
@@ -191,14 +221,18 @@ void HumanTurn(Board *b, int color)
 			continue;
 		}
 		
-		/* if no disks have been flipped */ 
 		{
-			int nflips = FlipDisks(m, b, color, 0, 1);
+			/* First check if disks have been flipped 
+				If they have, then perform actual move printing all flipped disks
+				else just ask the human to enter move again
+			*/ 
+			int nflips = FlipDisks(m, b, color, 0, 0);
 			if (nflips == 0) {
 				printf("Illegal move: no disks flipped\n");
 				PrintBoard(*b);
 				continue;
 			}
+
 			FlipDisks(m, b, color, 1, 1);
 			PlaceOrFlip(m, b, color);
 			printf("You flipped %d disks\n", nflips);
@@ -296,23 +330,39 @@ int findDifference(Board b, int color){
 	return CountBitsOnBoard(b, color) - CountBitsOnBoard(b, OTHERCOLOR(color));
 }
 
+/* check if this is the first iteration in the search tree
+	Usage - helps us to find if the move has to be skipped by the player when there is no legal move left
+*/
 bool isStartMove(Move m){
 	return (m.row == -1 and m.col == -1);
 }
 
+/* finds best move by the computer.
+	b - current board config
+	parent_move - what was the move earlier to this iteration.
+	color - color of the player
+	rem_moves - remaining iterations to search for 
+	best_diff - cilk reducer (passed by the parent call) which stores the best difference found by this iteration.
+		The reducer stores the parent_move and the difference. 
+	mul - factor by which the best diff has to be multiplied to return the diff to the parent. This is in 
+		context with negamax algorithm. The max for player is -1*(max of opponent) assuming both play optimally. 
+*/
 void findBestMove(Board b, Move parent_move, int color, int rem_moves, cilk::reducer_max<pair<Move,int>, MoveComparison> &best_diff, int verbose, int mul){
 
 	Board legal_moves;
 
 	int num_moves = EnumerateLegalMoves(b, color, &legal_moves);
 
-	int max_diff = -65;
-
 	if((b.disks[color] | b.disks[OTHERCOLOR(color)]) == ULLONG_MAX  || rem_moves == 0) {
 		best_diff.calc_max({parent_move, findDifference(b,color)});
 		return;
 	}
-		
+	
+	/* if there are no legal moves left then there can be 3 cases:
+		1. Even the first turn cannot be played by the player. In this we just skip.
+		2. Keep on searching the next best move by the opponent and try to minimize this. 
+		3. If there are no moves left even for opponent then the game has to end.
+	*/	
 	if(num_moves == 0LL) {	
 		if(isStartMove(parent_move)){
 			return;
@@ -330,6 +380,7 @@ void findBestMove(Board b, Move parent_move, int color, int rem_moves, cilk::red
 
 	cilk::reducer_max<pair<Move, int>, MoveComparison> best_child_diff;	
 
+	/* for every legal move try the negamax algorithm*/
 	for(int row = 8; row >=1; row--) {
 		for(int col = 8; col >=1; col--) {
 			// legal move exists
@@ -350,7 +401,10 @@ void findBestMove(Board b, Move parent_move, int color, int rem_moves, cilk::red
 			}
 		}
 	}
+
 	cilk_sync;
+	
+	/* store the corresponding moves and difference*/
 	int diff = best_child_diff.get_value().second * mul;
 
 	if(isStartMove(parent_move)){
@@ -365,11 +419,14 @@ void ComputerTurn(Board *b, Player *player)
 
 	int color = player->color;
 	cilk::reducer_max<pair<Move, int>, MoveComparison> best_diff;  
+	
+	/* start case when best move is unknown or not possible */
 	Move best_move = {-1,-1};
 
 	findBestMove(*b, best_move, color, player->depth, best_diff, 1,1);
 	best_move = best_diff.get_value().first;
 
+	/* if the best move is not possible then skip turn else print it*/
 	if(isStartMove(best_move)){
 		printf("No legal move left for Player %d. Skipping turn\n", color+1);
 		player->move_possible = false;
@@ -386,6 +443,11 @@ void ComputerTurn(Board *b, Player *player)
 	PrintBoard(*b);
 }
 
+
+/* evaluate the inputs given to the program
+		- The search depth should be within range (1-60)
+		- The player types should be either h or c
+*/
 bool EvaluateInputs(Player p1, Player p2){
 	bool result = true;
 	if(p1.type != COMPUTER and p1.type != HUMAN){
@@ -415,6 +477,13 @@ void TakeTurn(Board *gameboard, Player *p){
 	else HumanTurn(gameboard, p->color);
 }
 
+
+/*	1. Ask for inputs
+		2. Evaluate them
+		3. Make the players play their move else skip
+		4. End the game if no legal move exists for both players and print final result
+		5. print the time for the game execution along with number of workers. 
+*/
 int main (int argc, const char * argv[]) 
 {
 	clock_t t;
