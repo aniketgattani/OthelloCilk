7fedd5e578d105db2f6d72a2bf8f66b6fe8ec317
Aniket Gattani <aniketgattani@gmail.com>
2021-09-28 15:17:37 -0500

    Revert "use different algo"
    This reverts commit 807f3ef91fac7edb33f5c5bf5534fab86d5d1d81.

--- othello.cpp
+++ othello.cpp
@@ -396,189 +396,6 @@ int findBestMove(Board b, int color, int depth, int search_depth, int verbose, i
 	return best_diff.get_value() * mul;	
 }
 
-int GoodAITurn(Board *b, int color)
-{
-	Board legal_moves;
-	int num_moves = EnumerateLegalMoves(*b, color, &legal_moves);
-
-	if (num_moves > 0) {
-
-		int depth = 0;
-		Tuple t = max(num_moves, *b, legal_moves, color, depth);
-		int bestMove = t.bit;
-
-		// printf("bestmove: %d\n", bestMove);
-
-		Move m = BIT_TO_MOVE(bestMove);
-
-		int nflips = FlipDisks(m, b, color, 0, 1);
-		if (nflips == 0) {
-		    printf("Illegal move: no disks flipped!!\n");
-		}
-		PlaceOrFlip(m, b, color);
-		printf("Move by Player %d as 'row,col': %d %d \n", color + 1, m.row, m.col);
-		// PrintBoard(*b);
-		return 1;
-	}
-	else 
-		return 0;
-}
-
-Tuple max(int num_moves, Board b, Board legal_moves, int color, int depth)
-{
-	ull moves = legal_moves.disks[color];
-	ull moves_arr[num_moves];
-	for (int i = 0; i < num_moves; i++) 
-	{
-		moves_arr[i] = moves;
-	}
-
-	Tuple tuple_arr[num_moves];
-	int nextdepth = depth + 1;
-
-	// byte();
-	// binaryULL(moves);
-
-	cilk_for (int i = 0; i < num_moves; i++) 
-	{
-		ull moves_i = moves_arr[i];
-		Board tempB = b;
-		Tuple tempT;
-
-		int highestBit;
-		for (int j = 0; j < i + 1; j++)
-		{
-			highestBit = __builtin_clzll(moves_i);
-			moves_i -= ((ull)1) << (63-highestBit);
-		}
-		Move m = BIT_TO_MOVE(highestBit);
-
-		if (depth < DEPTH)
-		{
-			tempT.bit = highestBit;
-			tempT.flip = FlipDisks(m, &tempB, color, 0, 1);
-			PlaceOrFlip(m, &tempB, color);
-			tuple_arr[i] = tempT;
-
-			// PrintBoard(tempB);
-			// printf("max, movenum: %d movebit: %d \n\n", i, highestBit);
-
-			if (nextdepth < DEPTH)
-			{
-				Board next_legal_moves;
-				int other_color = OTHERCOLOR(color);
-				int num_moves = EnumerateLegalMoves(tempB, other_color, &next_legal_moves);
-				if (num_moves > 0) 
-				{
-					Tuple newT = min(num_moves, tempB, next_legal_moves, other_color, nextdepth);
-					tuple_arr[i].flip += newT.flip;
-				}
-			}
-		}
-	}
-
-	// printf("max\n");
-	// printTupleArray(tuple_arr, num_moves);
-	// printf("\n");
-
-	return findMaxBit(tuple_arr, num_moves);
-}
-
-Tuple min(int num_moves, Board b, Board legal_moves, int color, int depth)
-{
-	ull moves = legal_moves.disks[color];
-	ull moves_arr[num_moves];
-	for (int i = 0; i < num_moves; i++) 
-	{
-		moves_arr[i] = moves;
-	}
-
-	Tuple tuple_arr[num_moves];
-	int nextdepth = depth + 1;
-
-	// byte();
-	// binaryULL(moves);
-
-	cilk_for (int i = 0; i < num_moves; i++) 
-	{
-		ull moves_i = moves_arr[i];
-		Board tempB = b;
-		Tuple tempT;
-
-		int highestBit;
-		for (int j = 0; j < i + 1; j++)
-		{
-			highestBit = __builtin_clzll(moves_i);
-			moves_i -= ((ull)1) << (63-highestBit);
-		}
-		Move m = BIT_TO_MOVE(highestBit);
-
-		if (depth < DEPTH)
-		{
-			tempT.bit = highestBit;
-			tempT.flip = -(FlipDisks(m, &tempB, color, 0, 1));
-			PlaceOrFlip(m, &tempB, color);
-			tuple_arr[i] = tempT;
-
-			// PrintBoard(tempB);
-			// printf("min, movenum: %d movebit: %d \n\n", i, highestBit);
-
-			if (nextdepth < DEPTH)
-			{
-				Board next_legal_moves;
-				int other_color = OTHERCOLOR(color);
-				int num_moves = EnumerateLegalMoves(tempB, other_color, &next_legal_moves);
-				if (num_moves > 0) 
-				{
-					Tuple newT = max(num_moves, tempB, next_legal_moves, other_color, nextdepth);
-					tuple_arr[i].flip += newT.flip;
-				}
-			}
-		}
-	}
-
-	// printf("min\n");
-	// printTupleArray(tuple_arr, num_moves);
-	// printf("\n");
-
-	return findMinBit(tuple_arr, num_moves);
-}
-
-Tuple findMaxBit(Tuple arr[], int size) 
-{
-	int index = -1;
-	int temp = -64;
-
-	for (int i = 0; i < size; i++)
-    {
-    	int flip = arr[i].flip;
-        if (flip > temp)
-        {
-        	temp = flip;
-        	index = i;
-        }
-    }
-	return arr[index];
-}
-
-Tuple findMinBit(Tuple arr[], int size) 
-{
-	int index = -1;
-	int temp = 64;
-
-	for (int i = 0; i < size; i++)
-    {
-    	int flip = arr[i].flip;
-        if (flip < temp)
-        {
-        	temp = flip;
-        	index = i;
-        }
-    }
-	return arr[index];
-}
-
-
 void ComputerTurn(Board *b, Player *player)
 {
 
@@ -586,23 +403,23 @@ void ComputerTurn(Board *b, Player *player)
 	
 	/* start case when best move is unknown or not possible */
 	Move best_move = {-1,-1};
-	int res = GoodAITurn(b, player -> color);
-	// int best_diff= findBestMove(*b, color, 1, player->depth, 0, 1, true, best_move);
 
-	// /* if the best move is not possible then skip turn else print it*/
-	if(res==0){
+	int best_diff= findBestMove(*b, color, 1, player->depth, 0, 1, true, best_move);
+
+	/* if the best move is not possible then skip turn else print it*/
+	if(isStartMove(best_move)){
 		printf("No legal move left for Player %d. Skipping turn\n", color+1);
 		player->move_possible = false;
 		return;
 	}
 
-	// player->move_possible = true;
+	player->move_possible = true;
 
-	// int nflips = FlipDisks(best_move, b, color, 1, 1);
-	// PlaceOrFlip(best_move, b, color);
+	int nflips = FlipDisks(best_move, b, color, 1, 1);
+	PlaceOrFlip(best_move, b, color);
 
-	// printf("Move by Player %d as 'row,col': %d %d \n", color + 1, best_move.row, best_move.col);
-	// printf("Player %d flipped %d disks \n",  color+1, nflips);
+	printf("Move by Player %d as 'row,col': %d %d \n", color + 1, best_move.row, best_move.col);
+	printf("Player %d flipped %d disks \n",  color+1, nflips);
 	PrintBoard(*b);
 }
 
